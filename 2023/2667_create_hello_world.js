66e1f7440488e4c5ed13167fd8cd688bf1276eba
Chris Stephens <chrisjstephens@yahoo.com>
2023-05-23 16:49:37 -0400

    May 23 - leetcode solutions

---
+++ 2023/2667_create_hello_world.js
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/create-hello-world-function/description/
+//2667. Create Hello World Function
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
