3a4a30ea0568924682090a4cde3dcbfbdbe085fa
Chris Stephens <chrisjstephens@yahoo.com>
2023-05-24 17:14:44 -0400

    May 24 - leetcode solutions

---
+++ 2023/2619_array_prototype_last.js
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/array-prototype-last/description/
+//2619. Array Prototype Last
+Array.prototype.last = function() {
+   if (!this.length) return -1;
+
+   return this[this.length - 1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
---
+++ 2023/2665_counter_ii.js
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/counter-ii
+//2665. Counter II
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let val = init;
+    return {
+        increment: () => ++val,
+        decrement: () => --val,
+        reset: () => val = init
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
